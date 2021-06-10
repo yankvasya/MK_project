e2dabd55f170171f0bcd11148bd8cea0389bb06a
Vasily Yankovsky <yankvasya@gmail.com>
2021-06-10 10:04:53 +0300

    Сделал все, что было показано в видео

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
 const $arenas = document.querySelector('.arenas');
-const $randomButton = document.querySelector('.button');
+// const $randomButton = document.querySelector('.button');
+const $formFight = document.querySelector('.control');
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+const ATTACK = ['head', 'body', 'foot'];
 
 const player1 = {
     player: 1,
-    name: 'Ork',
+    name: 'IWillKillAllTitans',
     hp: 100,
-    img: 'https://i.gifer.com/YARx.gif',
+    img: './gif/eren.gif',
     weapon: ['Arms', 'Bomb', 'Pig'],
-    attack: function () {
-        console.log(this.name + ' Fight...');
-    },
-    changeHP: changeHP,
-    elHP: elHP,
-    renderHP: renderHP
+    attack,
+    changeHP,
+    elHP,
+    renderHP
 }
 
 const player2 = {
     player: 2,
-    name: 'Goblin',
+    name: 'Anime-Tyan',
     hp: 100,
-    img: 'https://i.gifer.com/Z23P.gif',
+    img: './gif/tyan.gif',
     weapon: ['Blade', 'Katana', 'Validol'],
     attack: function () {
         console.log(this.name + ' Fight...');
     },
-    changeHP: changeHP,
-    elHP: elHP,
-    renderHP: renderHP
+    changeHP,
+    elHP,
+    renderHP
+}
+
+function attack() {
+    console.log(this.name + ' Fight...');
 }
 
 
@@ -82,7 +91,6 @@ function playerWin(name) {
     } else {
         $winTitle.innerText = 'DRAW';
     }
-    createReloadButton();
     return $winTitle;
 }
 
@@ -90,36 +98,71 @@ function getRandom(num) {
     return Math.ceil(Math.random() * num);;
 }
 
-$randomButton.addEventListener('click', function () {
-    player1.changeHP(getRandom(20));
-    player1.renderHP();
-    player2.changeHP(getRandom(20));
-    player2.renderHP();
+// $randomButton.addEventListener('click', function () {
+//     player1.changeHP(getRandom(20));
+//     player1.renderHP();
+//     player2.changeHP(getRandom(20));
+//     player2.renderHP();
 
-    if (player1.hp === 0 || player2.hp === 0) {
-        $randomButton.disabled = true;
-    }
+//     if (player1.hp === 0 || player2.hp === 0) {
+//         $randomButton.disabled = true;
+//         createReloadButton();
+//     }
 
-    if (player1.hp === 0 && player1.hp < player2.hp) {
-        $arenas.appendChild(playerWin(player2.name));
-    } else if (player2.hp === 0 && player2.hp < player1.hp) {
-        $arenas.appendChild(playerWin(player1.name));
-    } else if (player1.hp === 0 && player2.hp === 0) {
-        $arenas.appendChild(playerWin())
-    }
-})
+//     if (player1.hp === 0 && player1.hp < player2.hp) {
+//         $arenas.appendChild(playerWin(player2.name));
+//     } else if (player2.hp === 0 && player2.hp < player1.hp) {
+//         $arenas.appendChild(playerWin(player1.name));
+//     } else if (player1.hp === 0 && player2.hp === 0) {
+//         $arenas.appendChild(playerWin())
+//     }
+// })
 
 
 function createReloadButton() {
     const $reloadWrap = createElement('div', 'reloadWrap');
     const $buttonRestart = createElement('button', 'button');
     $buttonRestart.innerText = 'Restart';
-    $arenas.appendChild($reloadWrap);
+
+    $buttonRestart.addEventListener('click', function () {
+        window.location.reload();
+    });
+
     $reloadWrap.appendChild($buttonRestart);
-    $buttonRestart.onclick = function () {
-        window.location.reload()
-    }
+    $arenas.appendChild($reloadWrap);
 }
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
+
+function enemyAttack() {
+    const hit = ATTACK[getRandom(3) - 1];
+    const defence = ATTACK[getRandom(3) - 1];
+
+    return {
+        value: getRandom(HIT[hit]),
+        hit,
+        defence,
+    }
+}
+
+$formFight.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const enemy = enemyAttack();
+    const attack = {};
+
+    for (let item of $formFight) {
+        if (item.checked && item.name === 'hit') {
+            attack.value = getRandom(HIT[item.value]);
+            attack.hit = item.value;
+        }
+
+        if (item.checked && item.name === 'defence') {
+            attack.defence = item.value;
+        }
+
+        item.checked = false;
+    }
+    console.log('a', attack);
+    console.log('e', enemy);
+})
