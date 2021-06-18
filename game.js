df9d879881ab32c0247328f528f695395cb6d5ef
Vasily Yankovsky <yankvasya@gmail.com>
2021-06-19 01:18:51 +0300

    Ошибка #55, попытка ее решить. С прошлой справился

--- game.js
+++ game.js
@@ -1,8 +1,5 @@
-// import { $formFight } from './creates.js';
-import generateLogs from './logs.js';
 import Player from './players.js';
-// import { enemyAttack, playerAttack } from './attack.js'
-// import { showResult } from './result.js'
+import { logs } from './constants.js';
 
 class Game {
     constructor() {
@@ -65,16 +62,33 @@ class Game {
         })
     }
 
+    generateLogs = (type, { name }, { name: playerName2, hp, pastDmg }) => {
+        let text = '';
+        const time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
+        switch (type) {
+            case 'start':
+                text = `${logs[type].replace('[time]', time).replace('[player1]', name).replace('[player2]', playerName2)}`;
+                break;
+            case 'hit':
+                text = `${time} | ${logs[type][getRandom(logs[type].length) - 1].replace('[playerKick]', name).replace('[playerDefence]', playerName2)} -${pastDmg} [${hp}/100]`;
+                break;
+            case 'defence':
+                text = `${time} | ${logs[type][getRandom(logs[type].length - 1)].replace('[playerKick]', playerName2).replace('[playerDefence]', name)} -${pastDmg} [${hp}/100]`;
+                break;
+            case 'draw':
+                text = `${time} ${logs[type]}`;
+                break;
+            case 'end':
+                text = `${time} ${logs[type][getRandom(logs[type].length - 1)].replace('playerWins', name).replace('playerLose', playerName2)}`;
+                break;
+            default:
+                text = 'Ошибка 404';
+                break;
+        }
+        const el = `<p>${text}</p>`;
+        this.$chat.insertAdjacentHTML('afterbegin', el);
+    }
+
 }
 
-
-
-
-
-
-
-// export {  };
 export default Game;
-// export { start };
-// export { start };
-// export start;
--- logs.js
+++ logs.js
@@ -3,31 +3,6 @@ import { getRandom } from './utils.js';
 import { logs } from './constants.js';
 // import Game from '.game.js'
 
-generateLogs = (type, { name }, { name: playerName2, hp, pastDmg }) => {
-    let text = '';
-    const time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
-    switch (type) {
-        case 'start':
-            text = `${logs[type].replace('[time]', time).replace('[player1]', name).replace('[player2]', playerName2)}`;
-            break;
-        case 'hit':
-            text = `${time} | ${logs[type][getRandom(logs[type].length) - 1].replace('[playerKick]', name).replace('[playerDefence]', playerName2)} -${pastDmg} [${hp}/100]`;
-            break;
-        case 'defence':
-            text = `${time} | ${logs[type][getRandom(logs[type].length - 1)].replace('[playerKick]', playerName2).replace('[playerDefence]', name)} -${pastDmg} [${hp}/100]`;
-            break;
-        case 'draw':
-            text = `${time} ${logs[type]}`;
-            break;
-        case 'end':
-            text = `${time} ${logs[type][getRandom(logs[type].length - 1)].replace('playerWins', name).replace('playerLose', playerName2)}`;
-            break;
-        default:
-            text = 'Ошибка 404';
-            break;
-    }
-    const el = `<p>${text}</p>`;
-    $chat.insertAdjacentHTML('afterbegin', el);
-}
 
-export default generateLogs;
+
+// export default generateLogs;
